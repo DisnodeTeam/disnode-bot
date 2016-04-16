57a6aa33490945e6789a8daa0acca89dc383e86b
AtecStudios <victoryforphil@gmail.com>
2016-04-16 00:38:28 -0700

    New Start

--- DisnodeLib/Disnode.js
+++
@@ -1,5 +0,0 @@
-var WolframAlpha = require("./WolframAlpha.js");
-var DiscordBot = require("./DiscordBot.js").botObject;
-
-module.exports.DiscordBot = DiscordBot;
-module.exports.WolframAlpha = WolframAlpha;
