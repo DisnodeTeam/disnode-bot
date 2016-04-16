052bc8c499bbc9f8a77ee342be89b87f0009d4ff
AtecStudios <victoryforphil@gmail.com>
2016-04-15 19:35:31 -0700

    Start 0f Command Handelr

--- DisnodeLib/DiscordBot.js
+++ DisnodeLib/DiscordBot.js
@@ -5,24 +5,33 @@ class DiscordBot{
   constructor(name, token){
     this.name = name;
     this.token = token;
-
   }
 
+  // Set On Ready Listener for Discord
   SetOnReady(onReady){
     this.OnReady = onReady;
   }
 
+  // Set On Message Listener for Discord
   SetOnMessage(_OnMessage){
     this.OnMessage = _OnMessage;
   }
 
   Start(){
     console.log("["+this.name+"] Starting Bot...");
+
     this.client = new Discord.Client();
     this.client.loginWithToken(this.token);
 
-    this.client.on("ready",this.OnReady);
-    this.client.on("message", this.OnMessage);
+    this.client.on("ready",()=>{
+      this.OnReady();
+    });
+
+    this.client.on("message",(msg)=>{
+      this.OnMessage(msg);
+    });
+
+
     console.log(this.name);
   }
   SendMessage(channel, msg){
