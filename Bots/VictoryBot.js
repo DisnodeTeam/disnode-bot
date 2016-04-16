a67d0916a5716c8b0666e0b6ce69d932fd4a2aed
AtecStudios <victoryforphil@gmail.com>
2016-04-15 19:24:48 -0700

    Base Commands

---
+++ Bots/VictoryBot.js
@@ -0,0 +1,10 @@
+var Disnode = require("../DisnodeLib/Disnode.js");
+var bot = new Disnode.DiscordBot("Victory Bot", "MTcwMDIwODA3MTk4NjM4MDgw.CfCntw.lUVQYtFJ-Jh2flq0-TXRUImjkZw");
+bot.SetOnReady(function(){
+  console.log("TESt");
+});
+
+bot.SetOnMessage(function(msg){
+  
+});
+bot.Start();
---
+++ DisnodeLib/Disnode.js
@@ -0,0 +1,5 @@
+var WolframAlpha = require("./WolframAlpha.js");
+var DiscordBot = require("./DiscordBot.js").botObject;
+
+module.exports.DiscordBot = DiscordBot;
+module.exports.WolframAlpha = WolframAlpha;
