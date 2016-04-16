f10b05af0202d9561208ee77004f0b43cf01f992
FireGamer3 <gm31334@gmail.com>
2016-04-15 19:54:44 -0500

    Project Begin
    start of Disnode!

---
+++ Disnode.js
@@ -0,0 +1,5 @@
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+
+//Will Handle Bot things in here "botname.js" could reference this file and use it to setup the bot 
+//Otherwise this could be all of the assets and each bot handles itself
