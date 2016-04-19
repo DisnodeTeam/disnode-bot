d6f1bc5189b28a57ab7083df4c39e26c88a1c473
FireGamer3 <gm31334@gmail.com>
2016-04-19 01:57:56 -0500

    [FireBot] YT Downloader

--- Bots/FireBot.js
+++ Bots/FireBot.js
@@ -1,11 +1,13 @@
 var Disnode = require("../DisnodeLib/Disnode.js");
 var Discord = require("Discord.js");
 var fs = require('fs');
+var walk = require('walk')
+var YoutubeMp3Downloader = require('youtube-mp3-downloader');
 
 var wimageid = "img";
 var wolframapi = require('wolfram-alpha').createClient("");
 
-var path = "../Audio/";
+var path = "C:/Users/Garrett/Desktop/FireBot/Audio/";
 var bot = new Discord.Client();
 var token = "";
 var name = "";
@@ -15,15 +17,26 @@ var commandPrefix = "#";
 var VoiceManager= new Disnode.VoiceManager(bot);
 var AudioPlayer= new Disnode.AudioPlayer(bot, fs);
 var wolfram= new Disnode.Wolfram(wolframapi);
+var YD = new YoutubeMp3Downloader({
+  "ffmpegPath": "C:/Users/Garrett/Desktop/FireBot/libmeg/bin/ffmpeg.exe", // Where is the FFmpeg binary located?
+  "outputPath": "C:/Users/Garrett/Desktop/FireBot/Audio/", // Where should the downloaded and encoded files be stored?
+  "youtubeVideoQuality": "highest", // What video quality should be used?
+  "queueParallelism": 2, // How many parallel downloads/encodes should be started?
+  "progressTimeout": 1000 // How long should be the interval of the progress reports
+});
+var ytManager = new Disnode.YoutubeManager(YD);
 
 var Commands = [
-	{cmd:"test", 	run: cmdTest,	desc: "Test Command"},
-	{cmd: "help",      run: cmdHelp,       desc: "List All Commands",     			usage:commandPrefix+"help"},
-	{cmd:"dc", 		run: cmdDC,		desc: "Disconnects the bot for shutdown",		usage:commandPrefix+"dc [Only FireGamer3 may use this command]"},
-	{cmd:"play", 	run: cmdPLAY,	desc: "Play audio clip",						usage:commandPrefix+"play [FileName ('-' instead of spaces.)]"},
-	{cmd:"jv", 		run: cmdJV,		desc: "Joins voice channel",					usage:commandPrefix+"jv [Voice Channel Name ('-' instead of spaces.)]"},
-	{cmd:"lv", 		run: cmdLV,		desc: "Leaves voice channel",					usage:commandPrefix+"lv [Voice Channel Name ('-' instead of spaces.)]"},
-	{cmd:"wa", 		run: cmdWA,		desc: "Pulls Information from Wolfram Alpha",	usage:commandPrefix+"wa [option1 option2] [Options: int (e.g. 1,2,3) or " + wimageid + " for imgaes]"},
+	{cmd:"test", 	run: cmdTest,		desc: "Test Command"},
+	{cmd: "help",   run: cmdHelp,       desc: "List All Commands",     					usage:commandPrefix+"help"},
+	{cmd:"dc", 		run: cmdDC,			desc: "Disconnects the bot for shutdown",		usage:commandPrefix+"dc [Only FireGamer3 may use this command]"},
+	{cmd:"play", 	run: cmdPLAY,		desc: "Play audio clip",						usage:commandPrefix+"play [FileName ('-' instead of spaces.)]"},
+	{cmd:"jv", 		run: cmdJV,			desc: "Joins voice channel",					usage:commandPrefix+"jv [Voice Channel Name ('-' instead of spaces.)]"},
+	{cmd:"lv", 		run: cmdLV,			desc: "Leaves voice channel",					usage:commandPrefix+"lv [Voice Channel Name ('-' instead of spaces.)]"},
+	{cmd:"wa", 		run: cmdWA,			desc: "Pulls Information from Wolfram Alpha",	usage:commandPrefix+"wa [option1 option2] [Options: int (e.g. 1,2,3) or " + wimageid + " for imgaes]"},
+	{cmd: "yt",     run: cmdDownloadYT, desc: "Download Youtube Clip", 					usage:commandPrefix+"yt [Video ID] [Command/Clip Name]"},
+	{cmd: "stop",   run: cmdStop,       desc: "Stop Audio Clip",      					usage:commandPrefix+"stop"},
+	{cmd: "list",   run: cmdListAudio,  desc: "List All Audio Clips",  					usage:commandPrefix+"list"},	
 ];
 var CommandHandler = new Disnode.CommandHandler(commandPrefix, Commands);
 
@@ -98,5 +111,57 @@ function cmdWA(msg, parms){
 		bot.updateMessage(wolfmsg, text);
 	});
 }
+function cmdDownloadYT(msg) {
+	var msg = msg.content;
 
+	var firstSpace =msg.indexOf(" ");
+	var link = msg.substring(firstSpace + 1, msg.indexOf(" ", firstSpace + 1));
+	var file = msg.substring(msg.indexOf(" ",msg.indexOf(link)) + 1,msg.length);
+
+	var progressMessage;
+
+
+
+	bot.sendMessage(msg.channel, "``` Video Code: "+link+" Command Name: "+file+"```" );
+	bot.sendMessage(msg.channel, "``` Downloading... ```", function(err, sent) {
+		progressMessage = sent;
+		console.log(err);
+	});
+
+	ytManager.SetOnFinished(function(data){
+		bot.updateMessage(progressMessage, "``` Finished. Use '!play "+file+"'```");
+	});
+	ytManager.SetOnError(function(error){
+		bot.updateMessage(progressMessage, error);
+	});
+	ytManager.SetOnProgess(function(progress){
+		console.log(progress.progress.percentage);
+		if(progress.progress.percentage != 100){
+			var percent = Math.round(progress.progress.percentage);
+			bot.updateMessage(progressMessage, "```Downloading..."+percent + "%```");
+		}
+	});
+	ytManager.Download(link, file);
+}
+function cmdListAudio(msg,parms){
+	var SendString = "``` === AUDIO CLIPS === \n";
+	AudioPlayer.listAll(walk,"./Audio/", function(name){
+		SendString = SendString + "-"+name+ "\n";
+		console.log(name);
+	}, function(){
+		SendString = SendString + "```";
+		bot.sendMessage(msg.channel, SendString);
+	});
+}
+function cmdStop(msg, parms){
+		//checks to see if user is in a voice channel
+	if (bot.voiceConnection){
+		//checks to see if they are in the same channel
+		bot.sendMessage(msg.channel, "``` Stopping Audio Playback```");
+		//path is the path to the audio directory
+		AudioPlayer.stopPlaying(bot);
+	}else {
+		bot.sendMessage(msg.channel, "``` Bot is not connected to a voice channel ```");
+	}
+}
 StartBot();
