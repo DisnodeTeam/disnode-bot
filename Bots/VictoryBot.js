64b9899ff5d15391665b579ae01fac4ee4f3cf93
FireGamer3 <gm31334@gmail.com>
2016-05-26 19:13:36 -0500

    Change your damn key Kappa

--- Bots/VictoryBot.js
+++ Bots/VictoryBot.js
@@ -7,7 +7,7 @@ var wolframapi = require('wolfram-alpha').createClient("L7GEPP-V87J3T9WL6");
 var fs = require('fs');
   var walk = require('walk')
 var bot = new Discord.Client();
-var token = "MTcwMDIwODA3MTk4NjM4MDgw.CfdSKQ.3mA4PpT_6OhNQTt4Pis0pWtlZvk";
+var token = "Change your Key Kappa";
 var name = "";
 var avatar = "";
 var BotChat;
