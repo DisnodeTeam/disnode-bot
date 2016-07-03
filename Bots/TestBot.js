a5576ae34cd4c4120f0f9f9d043f6c762d9879b9
AtecStudios <victoryforphil@gmail.com>
2016-07-02 21:22:28 -0700

    Test Bot Added. Rip Old Bots.

--- Bots/TestBot.js
+++ Bots/TestBot.js
@@ -2,40 +2,34 @@ var DiscordBot = require("../DisnodeLib/DiscordBot.js");
 var bot = new DiscordBot("");
 var botCommands = {};
 bot.on("Bot_Ready", function(){
-    console.log('[FB - BotReady] Bot Ready.');
+    console.log('[TEST_BOT - BotReady] Bot Ready.');
     bot.enableVoiceManager({voiceEvents:true});
     bot.enableAudioPlayer({path: './Audio/', maxVolume:2.0});
-    bot.enableConfigManager({path:"./FireBotConfig.json"});
+    bot.enableConfigManager({path:"./TestBotConfig.json"});
     bot.config.manager.loadConfig(OnLoad);
 });
 var OnLoad = function(){
   bot.enableCleverManager({channelid:"185614233168248833"});
   bot.enableWolfram({key:"a"});
   bot.enableYoutubeManager();
-  bot.enableCommandHandler({prefix: "#");
-  bot.command.handler.AddContext(botCommands,"firebot");
+  bot.enableCommandHandler({prefix: "#"});
+  bot.command.handler.AddContext(botCommands,"testbot");
   bot.command.handler.LoadList(bot.config.manager.config.commands)
 }
 
 bot.on("Bot_Init", function () {
-  console.log("[FB - BotReady] Bot Init.");
+  console.log("[TEST_BOT - BotReady] Bot Init.");
 });
 
 
-
 bot.on("Bot_RawMessage", function(msg){
-  console.log("[FB - RawMessage] Recieved Raw msg: " + msg.content);
+  console.log("[TEST_BOT - RawMessage] Recieved Raw msg: " + msg.content);
 });
 
 exports.Start = function () {
   bot.startBot();
-
 };
-botCommands.cmdDC = function(msg){
-  if (msg.msg.author.name =="FireGamer3"){
-		bot.bot.sendMessage(msg.msg.channel, "``` Disconnecting ```");
-		bot.bot.logout();
-	}else {
-		bot.bot.sendMessage(msg.channel, "I can't let you do that! " + msg.author);
-	}
+
+botCommands.cmdTestContext = function(msg){
+  bot.bot.sendMessage(msg.msg.channel, "THIS WAS CALLED IN THIS CONTEXT " );
 }
--- RunBots.js
+++ RunBots.js
@@ -1,5 +1,3 @@
 // This is the main script that runs all bots
-var VictoryBot = require('./Bots/VictoryBot.js');
-VictoryBot.Start();
-var TipBot = require('./Bots/TipBot.js');
-TipBot.Start();
+var TestBot = require('./Bots/TestBot.js');
+TestBot.Start();
