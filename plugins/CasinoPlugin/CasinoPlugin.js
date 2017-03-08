d6e9f5bc6b94653952f060e4c829cf5cc31bd1a5
AtecStudios <VictoryForPhil@gmail.com>
2017-03-07 22:01:48 -0800

    Message deleting

--- plugins/CasinoPlugin/CasinoPlugin.js
+++ plugins/CasinoPlugin/CasinoPlugin.js
@@ -6,6 +6,11 @@ class CasinoPlugin {
     var self = this;
     self.disnode.bot.SendCompactEmbed(command.msg.channel, "Test", "Test");
   }
+
+  commandSlot (command) {
+    var self = this;
+    self.disnode.bot.SendCompactEmbed(command.msg.channel, "Slot", "You bet was: " + command.params[0]);
+  }
 }
 
 module.exports = CasinoPlugin;
