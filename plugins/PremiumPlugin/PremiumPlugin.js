4e65a372b52e797d84580e4230627f7956d16713
AtecStudios <VictoryForPhil@gmail.com>
2017-04-01 01:07:16 -0700

    Prem Checking

--- plugins/PremiumPlugin/PremiumPlugin.js
+++ plugins/PremiumPlugin/PremiumPlugin.js
@@ -1,12 +1,28 @@
-class PingPlugin {
+class PremiumPlugin {
   constructor() {
 
   }
   default(command){ // This is the default command that is ran when you do your bot prefix and your plugin prefix example   !ping
-    this.disnode.bot.SendMessage(command.msg.channel, "PREM STATUS: " + GetUserRole(command.msg.userID));
+    var self = this;
+    self.GetUserRole(command.msg.userID).then(function(role){
+      self.disnode.bot.SendMessage(command.msg.channel, "PREM STATUS: " + role);
+    })
+
   }
   GetUserRole(userID){
-    return "ULTRA";
+      var self = this;
+    return new Promise(function(resolve, reject) {
+
+      self.disnode.DB.Find("users", {discordID: userID}).then(function(res){
+        console.log(res);
+        if(res[0]){
+            resolve(res[0].role);
+        }else{
+          resolve("USER_NOT_FOUND");
+        }
+      });
+
+    });
   }
 }
-module.exports = PingPlugin;
+module.exports = PremiumPlugin;
