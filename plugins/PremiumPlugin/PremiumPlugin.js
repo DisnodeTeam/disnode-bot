dc884456c907dacd67d81d235a3c66b5facb74c8
AtecStudios <VictoryForPhil@gmail.com>
2017-04-04 20:08:23 -0700

    Prem Check

--- plugins/PremiumPlugin/PremiumPlugin.js
+++ plugins/PremiumPlugin/PremiumPlugin.js
@@ -5,6 +5,10 @@ class PremiumPlugin {
   default(command){ // This is the default command that is ran when you do your bot prefix and your plugin prefix example   !ping
     var self = this;
     self.GetUserRole(command.msg.userID).then(function(role){
+      if(role == "USER_NOT_FOUND"){
+          self.disnode.bot.SendMessage(command.msg.channel, "You are not signed up on the DisnodeTeam website! (could not find user in DB)");
+        return;
+      }
       self.disnode.bot.SendMessage(command.msg.channel, "PREM STATUS: " + role);
     })
 
