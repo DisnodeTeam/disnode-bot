90b80e3c00616906ad0dcac01e4eb0077d3f688c
HazedSPaCEx <livewire192@gmail.com>
2017-04-11 22:13:35 -0700

    Changed Disnode Plugin Format

--- plugins/DisnodePlugin/DisnodePlugin.js
+++ plugins/DisnodePlugin/DisnodePlugin.js
@@ -13,46 +13,6 @@ class DisnodePlugin {
                 inline: false,
                 value: "Bot Info and Stats",
             }, {
-                name: 'Uptime',
-                inline: false,
-                value: self.disnode.stats.getUptime(),
-            }, {
-                name: 'Total Messages',
-                inline: true,
-                value: self.disnode.stats.messages,
-            }, {
-                name: 'Commands',
-                inline: true,
-                value: self.disnode.stats.messagesParsed,
-            }, {
-                name: '𝅳 𝅳𝅳 𝅳𝅳𝅳 𝅳𝅳𝅳',
-                inline: true,
-                value: '𝅳 𝅳𝅳 𝅳𝅳𝅳 𝅳𝅳𝅳',
-            }, {
-                name: 'Servers',
-                inline: true,
-                value: self.disnode.stats.serverCount,
-            }, {
-                name: 'Members',
-                inline: true,
-                value: self.disnode.stats.memberCount,
-            }, {
-                name: '𝅳 𝅳𝅳 𝅳𝅳𝅳 𝅳𝅳𝅳',
-                inline: true,
-                value: '𝅳 𝅳𝅳 𝅳𝅳𝅳 𝅳𝅳𝅳',
-            }, {
-                name: 'Channels',
-                inline: true,
-                value: self.disnode.stats.channelCount,
-            }, {
-                name: 'Open DM\'s',
-                inline: true,
-                value: self.disnode.stats.directMessageCount,
-            }, {
-                name: '𝅳 𝅳𝅳 𝅳𝅳𝅳 𝅳𝅳𝅳',
-                inline: true,
-                value: '𝅳 𝅳𝅳 𝅳𝅳𝅳 𝅳𝅳𝅳',
-            }, {
                 name: 'Shard #',
                 inline: true,
                 value: self.disnode.bot.shardID + ' of ' + self.disnode.botConfig.shardCount,
@@ -61,9 +21,33 @@ class DisnodePlugin {
                 inline: true,
                 value: '' + self.disnode.botConfig.shardMode,
             }, {
-                name: '𝅳 𝅳𝅳 𝅳𝅳𝅳 𝅳𝅳𝅳',
+                name: 'Uptime',
                 inline: true,
-                value: '𝅳 𝅳𝅳 𝅳𝅳𝅳 𝅳𝅳𝅳',
+                value: self.disnode.stats.getUptime(),
+            }, {
+                name: 'Servers',
+                inline: true,
+                value: self.disnode.stats.serverCount,
+            }, {
+                name: 'Channels',
+                inline: true,
+                value: self.disnode.stats.channelCount,
+            }, {
+                name: 'Commands',
+                inline: true,
+                value: self.disnode.stats.messagesParsed,
+            }, {
+                name: 'Members',
+                inline: true,
+                value: self.disnode.stats.memberCount,
+            }, {
+                name: 'Open DM\'s',
+                inline: true,
+                value: self.disnode.stats.directMessageCount,
+            }, {
+                name: 'Total Messages',
+                inline: true,
+                value: self.disnode.stats.messages,
             }],
             footer: {}
         });
