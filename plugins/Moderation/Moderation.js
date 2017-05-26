814bce4f45b0b4a7b5a9d3ecdc0b7200ffce64bf
HazedSPaCEx <livewire192@gmail.com>
2017-05-26 16:42:36 -0700

    updated mod plugin to workwith pluginmanager crap and fixed role name

--- plugins/Moderation/Moderation.js
+++ plugins/Moderation/Moderation.js
@@ -1,3 +1,6 @@
+const sleep = require('system-sleep');
+const Discord = require('discord.io');
+
 class ModerationPlugin {
   constructor() {}
   default (command) {
@@ -20,8 +23,8 @@ class ModerationPlugin {
   helplist(command) {
     var self = this;
     var msg = "";
-    for (var i = 0; i < self.class.commands.length; i++) {
-      msg += "**" + self.disnode.botConfig.prefix + self.class.config.prefix + " " + self.class.commands[i].cmd + "** - " + self.class.commands[i].desc + "\n";
+    for (var i = 0; i < self.commands.length; i++) {
+      msg += "**" + self.disnode.botConfig.prefix + self.config.prefix + " " + self.commands[i].cmd + "** - " + self.commands[i].desc + "\n";
     }
     return msg;
   }
@@ -91,9 +94,60 @@ class ModerationPlugin {
       if (command.params[0] == undefined) {
         self.disnode.bot.SendMessage(command.msg.channel, ":warning: No user has been inputed.");
       } else {
+        var server = this.disnode.bot.GetServerByID(command.msg.server);
         var uid = command.params[0];
         uid = uid.replace(/\D/g, '');
-        self.disnode.bot.Mute(command.msg.server, uid);
+        var role;
+        var found = false;
+        for (var key in server.roles) {
+          if (self.disnode.bot.GetBotInfo().username + "-Muted" == server.roles[key].name) {
+            role = server.roles[key];
+            found = true;
+            break;
+          }
+        }
+        if (found) {
+          var id = role.id;
+          self.disnode.bot.client.addToRole({
+            serverID: command.msg.server,
+            userID: uid,
+            roleID: id
+          });
+        } else {
+          self.disnode.bot.client.createRole(command.msg.server, function(err, resp) {
+            if (err) return console.log(err);
+            self.disnode.bot.client.editRole({
+              serverID: command.msg.server,
+              roleID: resp.id,
+              name: self.disnode.bot.GetBotInfo().username + "-Muted",
+              color: 0x525252,
+              hoist: true,
+              permissions: {
+                TEXT_READ_MESSAGES: true,
+                TEXT_SEND_MESSAGES: false,
+                VOICE_CONNECT: true,
+                VOICE_SPEAK: false,
+              },
+              mentionable: false,
+              position: 5
+            });
+            var channels = Object.keys(self.disnode.bot.client.servers[command.msg.server].channels);
+            for (var i = 0; i < channels.length; i++) {
+              var channel = self.disnode.bot.client.servers[command.msg.server].channels[channels[i]];
+              if (channel.type != "text") continue;
+              self.disnode.bot.client.editChannelPermissions({
+                channelID: channel.id,
+                roleID: resp.id,
+                deny: [Discord.Permissions.TEXT_SEND_MESSAGES, Discord.Permissions.TEXT_ATTACH_FILES],
+              });
+            }
+            self.disnode.bot.client.addToRole({
+              serverID: command.msg.server,
+              userID: uid,
+              roleID: resp.id
+            });
+          });
+        }
         self.disnode.bot.SendMessage(command.msg.channel, "User **" + self.U2ID(command) + "** - (" + uid + ") has been muted.");
       }
     } else self.AccessDenied(command);
@@ -104,10 +158,53 @@ class ModerationPlugin {
       if (command.params[0] == undefined) {
         self.disnode.bot.SendMessage(command.msg.channel, ":warning: No user has been inputed.");
       } else {
+        var server = this.disnode.bot.GetServerByID(command.msg.server);
         var uid = command.params[0];
         uid = uid.replace(/\D/g, '');
-        self.disnode.bot.Unmute(command.msg.server, uid);
-        self.disnode.bot.SendMessage(command.msg.channel, "User **" + self.U2ID(command) + "** - (" + uid + ") has been unmuted.");
+        var role;
+        var found = false;
+        for (var key in server.roles) {
+          if ("Zira-Muted" == server.roles[key].name) {
+            role = server.roles[key];
+            found = true;
+            break;
+          }
+        }
+        if (found) {
+          var id = role.id;
+          self.disnode.bot.client.removeFromRole({
+            serverID: command.msg.server,
+            userID: uid,
+            roleID: id
+          });
+          self.disnode.bot.SendMessage(command.msg.channel, "User **" + self.U2ID(command) + "** - (" + uid + ") has been unmuted.");
+        } else {
+          self.disnode.bot.SendMessage(command.msg.channel, "No user has been muted yet on this server.")
+        }
+      }
+    } else self.AccessDenied(command);
+  }
+  commandDelete(command) {
+    var self = this;
+    if (command.msg.userID == self.guildOwner(command)) {
+      if (command.params[0] == undefined) {
+        self.disnode.bot.SendMessage(command.msg.channel, ":warning: Pick an amount of messages to delete. (1-100)");
+      } else {
+        self.disnode.bot.client.getMessages({
+          channelID: command.msg.channel,
+          limit: parseInt(command.params[0]),
+          before: command.msg.obj.id,
+        }, function(error, messageArry) {
+          var ids = [];
+          for (var i = 0; i < messageArry.length; i++) {
+            ids.push(messageArry[i].id);
+          }
+          self.disnode.bot.DeleteMessages(command.msg.channel, ids);
+          self.disnode.bot.client.deleteMessage({
+            channelID: command.msg.channel,
+            messageID: command.msg.obj.id
+          });
+        });
       }
     } else self.AccessDenied(command);
   }
