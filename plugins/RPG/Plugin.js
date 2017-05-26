418431e79f0be5d4fd8aac758b877ae08fa4c008
HazedSPaCEx <livewire192@gmail.com>
2017-05-26 10:32:37 -0700

    Base of it

---
+++ plugins/RPG/Plugin.js
@@ -0,0 +1,30 @@
+class Template {
+  constructor() {}
+  default(command){
+    var self = this;
+    var msg = "";
+    for (var i = 0; i < self.class.commands.length; i++) {
+      msg += self.disnode.botConfig.prefix + self.class.config.prefix + " " + self.class.commands[i].cmd + " - " + self.class.commands[i].desc + "\n";
+    }
+    self.disnode.bot.SendEmbed(command.msg.channel, {
+      color: 3447003,
+      author: {},
+      fields: [ {
+        name: 'RPG',
+        inline: true,
+        value: "Hello, " + command.msg.user + "!",
+      },{
+        name: 'Commands:',
+        inline: true,
+        value: msg,
+      }, {
+        name: 'Discord Server',
+        inline: false,
+        value: "**Join the Disnode Server for Support and More!:** https://discord.gg/AbZhCen",
+      }],
+        footer: {}
+    });
+  }
+
+}
+module.exports = Template;
