2bcef466e38e599e64cc44266900493ca6d07833
Alex Carter <accounts@atecstudios.com>
2017-10-05 20:24:21 -0700

    Some CAH Work

---
+++ plugins/CAH/Plugin.js
@@ -0,0 +1,18 @@
+const Session = require('./Session.js');
+const Axios = require("axios")
+class Giveaway {
+  constructor() {
+    this.sessions = [];
+  }
+  default(command){
+    var self = this;
+    var msg = "";
+   
+  }
+  commandNew(command){
+    var self = this;
+    axios.post("cah.")
+  }
+  
+}
+module.exports = Giveaway;
