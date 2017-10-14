d17eeae002f41005c9eea30745d8999b22c7fc1f
Alex Carter <accounts@atecstudios.com>
2017-10-14 15:30:33 -0700

    CAH - Joining a Game

--- plugins/CAH/Plugin.js
+++ plugins/CAH/Plugin.js
@@ -1,8 +1,15 @@
 const Session = require('./Session.js');
-const Axios = require("axios")
-class Giveaway {
+const axios = require("axios")
+
+const ENDPOINT = "http://127.0.0.1:9999"
+class CAH {
   constructor() {
     this.sessions = [];
+    this.request = axios.create({
+      baseURL: ENDPOINT,
+      timeout: 2500,
+      headers: {'auth': 'FlWo39AEgJuw8hvl6diIqcekictExUbcqkMV16DPGJAXdz4v71Lzm2QBJUo5M2gU'}
+    });
   }
   default(command){
     var self = this;
@@ -11,8 +18,18 @@ class Giveaway {
   }
   commandNew(command){
     var self = this;
-    axios.post("cah.")
+    var creatorObject = {
+      creator: command.msg.user.id
+    }
+   
+    self.request.post(ENDPOINT + "/game", creatorObject).then((res)=>{
+      var GameCode = res.data.data.toUpperCase();
+      self.disnode.bot.SendCompactEmbed(command.msg.channelID, "Game Created!","**"+ command.msg.user.username + "** created a game! \nCode: `" + GameCode + "`");
+    }).catch((err)=>{
+      
+      self.disnode.bot.SendMessage(command.msg.channelID, JSON.stringify(err.response.data.message));
+    });
   }
   
 }
-module.exports = Giveaway;
+module.exports = CAH;
